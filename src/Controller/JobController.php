bf5ad4fa16e52e5a339330e1ce30aa155b1b4a2c
Lifdas <florian.potier-clemente@e-cdp.com>
2025-06-05 14:52:20 +0200

    suppression d'un import en double

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -6,7 +6,6 @@ use App\Entity\Job;
 use App\Repository\JobCategorieRepository;
 use App\Repository\JobRepository;
 use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
